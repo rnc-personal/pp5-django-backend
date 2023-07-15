d3d5483e5decc283ab064320ca9e9d3fb94165ea
rnc-personal <richard.challenger@protonmail.com>
2023-07-15 13:19:52 +0000

    TEMP Commit: Issue with Rating PUT

--- ratings/serializers.py
+++ ratings/serializers.py
@@ -2,6 +2,9 @@ from django.db import IntegrityError
 from rest_framework import serializers
 from ratings.models import Rating
 
+from rest_framework import serializers
+from ratings.models import Rating
+
 class RatingSerializer(serializers.ModelSerializer):
     creator = serializers.ReadOnlyField(source='creator.username')
     score = serializers.SerializerMethodField()
@@ -11,28 +14,16 @@ class RatingSerializer(serializers.ModelSerializer):
         request = self.context['request']
         if request.user == obj.creator:
             return obj.score
-        else:
-            return 0
+        return None
 
     def get_average_rating(self, obj):
-            return obj.build.average_rating
+        return obj.build.average_rating
 
     def validate_score(self, value):
         request = self.context['request']
         if self.instance and request.user == self.instance.creator:
             return value
         raise serializers.ValidationError("You can only edit your own score.")
-
-    def create(self, validated_data):
-        try:
-            return super().create(validated_data)
-        except IntegrityError:
-            raise serializers.ValidationError('User already rated this post')
-
-    def update(self, instance, validated_data):
-        instance.score = validated_data.get('score', instance.score)
-        instance.save()
-        return instance
 
     class Meta:
         model = Rating
--- ratings/views.py
+++ ratings/views.py
@@ -1,15 +1,31 @@
-from rest_framework import generics, permissions
+from django.db import IntegrityError
+from rest_framework import generics, permissions, serializers
 from pp5djangoapi.permissions import IsOwnerOrReadOnly
 from ratings.models import Rating
 from ratings.serializers import RatingSerializer
 
+
 class RatingList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = RatingSerializer
-    queryset = Rating.objects.all()
+
+    def get_queryset(self):
+        queryset = Rating.objects.all()
+        if self.request.user.is_authenticated:
+            queryset = queryset.filter(creator=self.request.user)
+        return queryset
 
     def perform_create(self, serializer):
-        serializer.save(creator=self.request.user)
+            creator = self.request.user
+            build = serializer.validated_data.get('build')
+            try:
+                rating = Rating.objects.get(creator=creator, build=build)
+                rating.score = serializer.validated_data.get('score')
+                rating.save()
+            except Rating.DoesNotExist:
+                serializer.save(creator=creator)
+            except IntegrityError:
+                raise serializers.ValidationError('User already rated this post')
 
 class RatingDetail(generics.RetrieveDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
