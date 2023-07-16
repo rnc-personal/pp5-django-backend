ef8785d883bc193693fb690e05d641f7ffa9546a
rnc-personal <richard.challenger@protonmail.com>
2023-07-16 11:49:06 +0000

    ADD: boilerplate build generic view (likes removed)

--- builds/views.py
+++ builds/views.py
@@ -1,70 +1,50 @@
-from django.http import Http404
-from rest_framework import status, permissions
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
+# from django_filters.rest_framework import DjangoFilterBackend
+from pp5djangoapi.permissions import IsOwnerOrReadOnly
 from .models import Build
 from .serializers import BuildSerializer
-from pp5djangoapi.permissions import IsOwnerOrReadOnly
 
-class BuildList(APIView):
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly
+
+class BuildList(generics.ListCreateAPIView):
+    serializer_class = BuildSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Build.objects.annotate(
+        # likes_count=Count('likes', distinct=True),
+        comments_count=Count('comments', distinct=True)
+        ).order_by('-created_at')
+    
+    # filter_backends = [
+    #     filters.OrderingFilter,
+    #     filters.SearchFilter,
+    #     DjangoFilterBackend,
+    # ]
+
+    filterset_fields = [
+        'creator__followed__creator__profile',
+        # 'likes__creator__profile',
+        'creator__profile',
     ]
 
-    def get(self, request):
-        builds = Build.objects.all()
-        serializer = BuildSerializer(
-            builds, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
+    ordering_fields = [
+        # 'likes_count',
+        'comments_count',
+        # 'likes__created_at',
+    ]
 
-    def post(self, request):
-        serializer = BuildSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
+    search_fields = [
+        'creator__username',
+        'title',
+    ]
 
-class BuildDetail(APIView):
-    permission_classes = [IsOwnerOrReadOnly]
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
+
+
+class BuildDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = BuildSerializer
-
-    def get_object(self, pk):
-        try:
-            build = Build.objects.get(pk=pk)
-            self.check_object_permissions(self.request, build)
-            return build
-        except Build.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        build = self.get_object(pk)
-        serializer = BuildSerializer(
-            build, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        build = self.get_object(pk)
-        serializer = BuildSerializer(
-            build, data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
-
-    def delete(self, request, pk):
-        build = self.get_object(pk)
-        build.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Build.objects.annotate(
+        # likes_count=Count('likes', distinct=True),
+        comments_count=Count('comments', distinct=True)
+    ).order_by('-created_at')
