1abc6ee89ee41075317447d376995c8b3427e403
rnc-personal <richard.challenger@protonmail.com>
2023-07-16 11:56:13 +0000

    ADD: Profiles Generic View

--- profiles/views.py
+++ profiles/views.py
@@ -1,38 +1,45 @@
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from django.db.models import Count
+from rest_framework import generics, filters
+# from django_filters.rest_framework import DjangoFilterBackend
+from pp5djangoapi.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
-from pp5djangoapi.permissions import IsOwnerOrReadOnly
 
+class ProfileList(generics.ListAPIView):
+    queryset = Profile.objects.annotate(
+        builds_count=Count('creator__build', distinct=True),
+        followers_count=Count('creator__followed', distinct=True),
+        following_count=Count('creator__following', distinct=True)
+    ).order_by('-created_at')
 
-class ProfileList(APIView):
-    # TEMP: Will replace with generic later
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True, context={'request': request})
-        return Response(serializer.data)
-
-class ProfileDetail(APIView):
     serializer_class = ProfileSerializer
-    permission_classes = [IsOwnerOrReadOnly]
-    def get_object(self, pk):
-        try:
-            profile = Profile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
-    
-    def get(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, context={'request': request})
-        return Response(serializer.data)
 
-    def put(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # filter_backends = [
+    #     filters.OrderingFilter,
+    #     DjangoFilterBackend,
+    # ]
+
+    filterset_fields = [
+        'creator__following__followed__profile',
+        'creator__followed__creator__profile',
+    ]
+
+    ordering_fields = [
+        'builds_count',
+        'followers_count',
+        'following_count',
+        'creator__following__created_at',
+        'creator__followed__created_at',
+    ]
+
+
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+
+    queryset = Profile.objects.annotate(
+        builds_count=Count('creator__build', distinct=True),
+        followers_count=Count('creator__followed', distinct=True),
+        following_count=Count('creator__following', distinct=True)
+    ).order_by('-created_at')
+
+    serializer_class = ProfileSerializer
