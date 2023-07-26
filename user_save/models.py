64b1883615c491e799ef1ab7d4363aa2d09de5bc
rnc-personal <richard.challenger@protonmail.com>
2023-07-26 08:49:18 +0000

    ADD: Save Build Module

---
+++ user_save/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+from builds.models import Build
+
+class Like(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    build = models.ForeignKey(Build, related_name='saves', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['creator', 'build']
+
+    def __str__(self):
+        return f"{self.creator} saved {self.build}"
+    
