d8470e6831ae57113438b840a8efd4f5a24d5fe3
rnc-personal <richard.challenger@protonmail.com>
2023-07-26 09:00:50 +0000

    ADD: Views for Saved Builds List and Individual Saved Builds

--- user_save/views.py
+++ user_save/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from pp5djangoapi.permissions import IsOwnerOrReadOnly
+from user_save.models import SaveLike
+from user_save.serializers import SaveSerializer
 
-# Create your views here.
+class SaveList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = SaveSerializer
+    queryset = Save.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
+
+class SaveDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Save.objects.all()
