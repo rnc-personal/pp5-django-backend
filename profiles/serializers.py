d8c03ea9937408757673cf90d80bcc6e32af565d
rnc-personal <richard.challenger@protonmail.com>
2023-07-29 12:36:47 +0000

    FIX: Followers Not Being Displayed in API

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,14 +1,27 @@
 from rest_framework import serializers
 from .models import Profile
+from followers.models import Follower
 
 class ProfileSerializer(serializers.ModelSerializer):
     creator = serializers.ReadOnlyField(source='creator.username')
     is_creator = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
+    builds_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
 
     def get_is_creator(self, obj):
         request = self.context['request']
         return request.user == obj.creator
 
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                creator=user, followed=obj.creator
+            ).first()
+            return following.id if following else None
+        return None
 
     class Meta:
         model = Profile
